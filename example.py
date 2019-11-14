63404355f3a19ff75a986f234d1dc8deaeed5fdb
Bert Wagner <github@bertwagner.com>
2019-11-13 21:06:31 -0500

    Add code to update in azure table storage

--- example.py
+++ example.py
@@ -9,8 +9,16 @@ def GetData(account_name,access_key):
     # Table storage design https://docs.microsoft.com/en-us/rest/api/storageservices/designing-a-scalable-partitioning-strategy-for-azure-table-storage
     # table/entity design - 1mb entity limit, up to 252 key/value pair properties https://docs.microsoft.com/en-us/azure/cosmos-db/table-storage-overview
     table_service = TableService(account_name=account_name,account_key=access_key)
-    task = table_service.get_entity('season', '1', 'Bert')
+    
+    # Read a value
+    task = table_service.get_entity('league', 'bert@example.org', 'Volleyball 2018')
     test="a"
+
+    # Update a value
+    task = {'PartitionKey': 'bert@example.org', 'RowKey': 'Volleyball 2018',
+        'CreateDate': '2019-11-12T00:01:01.00Z'}
+    table_service.update_entity('league', task)
+    
 
 
 if __name__ == '__main__':
